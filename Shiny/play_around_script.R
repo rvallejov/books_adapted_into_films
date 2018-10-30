279ff3f15120de0d850e8a2b029587f1f3864cb3
rvallejov <rvallejovillafuerte@gmail.com>
2018-10-30 16:35:14 -0400

    scraping IMDb from wikipedia links

--- Shiny/play_around_script.R
+++ Shiny/play_around_script.R
@@ -5,14 +5,12 @@ library(ggplot2)
 library(tidyr)
 library(lubridate)
 library(stringr)
-# library(googleVis)
-# library(rsconnect)
 library(scales)
 
 # goodreads.data <- fread(file = "C:/Users/USUARIO/Documents/NYC DA/Python/NYC DA/goodreads/goodreads_awards.csv")
 goodreads.data <- fread(file = "goodreads_awards_backup.csv")
 goodreads.data.2 <- fread(file = "goodreads_books.csv")
-wiki.data <- fread(file = "wiki_books.csv")
+wiki.data <- fread(file = "wiki_books_new.csv")
 
 colnames(goodreads.data)[4] <- c("genre")
 
@@ -23,7 +21,8 @@ head(wiki.data)
 tail(wiki.data)
 
 wiki.data %>%
-  filter(str_count(book_title,"Harry Potter") > 0)
+  # filter(str_count(book_title,"Harry Potter") > 0)
+  filter(str_count(book_title,"Scarlet Letter") > 0)
 
 goodreads.data <- bind_rows(select(goodreads.data,-votes),goodreads.data.2)
 
@@ -73,34 +72,20 @@ books.into.films %>%
 books.into.films %>%
   filter(str_count(book_title,"Twilight") > 0)
 
+# Get the top genres of books by number of ratings
 top.categories <- goodreads.data %>%
                     group_by(category) %>%
                     summarise(ratings = sum(ratings),avg_rating = mean(avg_rating)) %>%
                     top_n(10, ratings)
 
+# Plot boxplots for the top 10 genre
 goodreads.data %>%
   filter(category %in% top.categories$category) %>%
   ggplot(aes(x=category,y=ratings)) +
   geom_boxplot()
 
-g <- books.into.films %>%
-  group_by(author,book_title) %>% 
-  summarise(book_year = max(book_year), 
-            avg_rating = max(avg_rating), 
-            ratings = max(ratings),
-            reviews = max(reviews),
-            adapted.film = max(adapted.film)) %>% 
-  ggplot(aes(x=avg_rating,y=log(ratings))) +
-  geom_point(aes(text=sprintf("Book: %s<br>Ratings: %s<br>Adapted: %s",
-                              book_title, comma(ratings), adapted.film),
-                 col=as.character(adapted.film))) + 
-  # theme(axis.text.x=element_blank(),
-  #       axis.ticks.x=element_blank(),
-  #       panel.grid.major = element_blank(),
-  #       panel.grid.minor = element_blank()) +
-  labs(title = paste0(length(unique(books.into.films$book_title))," books"))
-ggplotly(g,tooltip = "text") %>% config(displayModeBar = FALSE)
-
+# Relationship between avg_rating and log(ratings)
+# by adapted screenplays
 g <- books.into.films %>%
   filter(str_count(book_year,",") == 0, str_count(film_year,",") == 0 | is.na(film_year)) %>% 
   group_by(author,book_title) %>% 
@@ -112,16 +97,14 @@ g <- books.into.films %>%
   ggplot(aes(x=avg_rating,y=log(ratings))) +
   geom_point(aes(text=sprintf("Book: %s<br>Ratings: %s<br>Adapted: %s",
                               book_title, comma(ratings), adapted.film),
-                 col=as.character(adapted.film))) + 
-  # theme(axis.text.x=element_blank(),
-  #       axis.ticks.x=element_blank(),
-  #       panel.grid.major = element_blank(),
-  #       panel.grid.minor = element_blank()) +
+                 col=as.character(adapted.film)),
+             alpha = 0.5) + 
   labs(title = paste0(length(unique(books.into.films$book_title))," books"))
 ggplotly(g,tooltip = "text") %>% config(displayModeBar = FALSE)
 
+
 g <- books.into.films %>%
-  filter(str_count(book_year,",") == 0, str_count(film_year,",") == 0) %>% 
+  filter(str_count(book_year,",") == 0, str_count(film_year,",") == 0 | is.na(film_year)) %>% 
   group_by(author,book_title) %>% 
   summarise(book_year = max(book_year), 
             avg_rating = max(avg_rating), 
@@ -134,49 +117,13 @@ g <- books.into.films %>%
             reviews = sum(reviews),
             adapted.film = mean(adapted.film),
             n = n()) %>%
-  ggplot(aes(x=as.integer(book_year),y=n)) +
+  ggplot(aes(x=as.integer(book_year),y=ratings/n)) +
   geom_col(aes(color = adapted.film)) +
   scale_color_gradient(low = "blue", high = "red")
 ggplotly(g) %>% config(displayModeBar = FALSE)
 
-g <- books.into.films %>%
-  filter(str_count(book_year,",") == 0, str_count(film_year,",") == 0) %>% 
-  group_by(author,book_title) %>% 
-  summarise(book_year = max(book_year), 
-            avg_rating = max(avg_rating), 
-            ratings = max(ratings),
-            reviews = max(reviews),
-            adapted.film = max(adapted.film)) %>% 
-  group_by(book_year) %>%
-  summarise(ratings = sum(ratings),
-            avg_rating = mean(avg_rating),
-            reviews = sum(reviews),
-            adapted.film = mean(adapted.film),
-            n = n()) %>%
-  ggplot(aes(x=n,y=adapted.film)) +
-  geom_point(aes(color = as.integer(book_year))) +
-  scale_color_gradient(low = "blue", high = "red")
-ggplotly(g) %>% config(displayModeBar = FALSE)
-
-books.into.films %>% 
-  filter(str_count(book_year,",") == 0, str_count(film_year,",") == 0) %>% 
-  mutate(book_year = as.integer(book_year)) %>% 
-  group_by(author,book_title) %>% 
-  summarise(book_year = max(book_year), 
-            avg_rating = max(avg_rating), 
-            ratings = max(ratings),
-            reviews = max(reviews),
-            adapted.film = max(adapted.film)) %>% 
-  group_by(adapted.film) %>% 
-  summarise(book_year = mean(book_year), 
-            avg_rating = mean(avg_rating), 
-            ratings.p.book = mean(ratings),
-            total.ratings = sum(ratings),
-            reviews = mean(reviews),
-            n = n())
-
 books.into.films %>%
-  filter(str_count(film_year,",") > 0)
+  filter(str_count(book_year,",") == 0, str_count(film_year,",") == 0 | is.na(film_year))
 
 
 
--- wiki_books/wiki_books/spiders/wiki_books_spider.py
+++ wiki_books/wiki_books/spiders/wiki_books_spider.py
@@ -51,24 +51,97 @@ class Wiki_books(Spider):
                 film_title = book.xpath('.//td[2]/i/a/text()').extract()
                 if (film_title == []):
                     film_title = book.xpath('.//td[2]/i/text()').extract()
+                
+                film_wiki = list(map(lambda x: "".join(['https://en.wikipedia.org',x]),book.xpath('.//td[2]/i/a/@href').extract()))
 
-                for b_title,b_year in zip(book_title, book_year):
-                    for f_title,f_year in zip(film_title, film_year):
-                        item = WikiBooksItem()
-                        item['book_title'] = b_title
-                        item['book_year'] = b_year
-                        item['book_author'] = book_author
-                        item['film_title'] = f_title
-                        item['film_year'] = f_year
-                        yield item
+                print(len(film_wiki))
+                print('*'*50)
+                for url,f_title,f_year in zip(film_wiki,film_title, film_year):
+                    if (url != [] or re.search('[^(en)]\.wikipedia',url) == None):
+                        yield Request(url=url, callback=self.parse_film_wiki,
+                                        meta={'book_title': book_title,
+                                        'book_year': book_year,
+                                        'book_author': book_author,
+                                        'f_title': f_title,
+                                        'f_year': f_year})
+                # for b_title,b_year in zip(book_title, book_year):
+                #     for f_title,f_year in zip(film_title, film_year):
+                #         item = WikiBooksItem()
+                #         item['book_title'] = b_title
+                #         item['book_year'] = b_year
+                #         item['book_author'] = book_author
+                #         item['film_title'] = f_title
+                #         item['film_year'] = f_year
+                #         yield item
 
-                # item = WikiBooksItem()
-                # item['book_title'] = list(map(lambda x: [x],book_title))
-                # item['book_year'] = book_year
-                # item['book_author'] = book_author
-                # item['film_title'] = film_title
-                # item['film_year'] = film_year
-                # yield item
             except:
                 print("Next letter")
                 print('-'*50)
+
+    def parse_film_wiki(self,response):
+        book_title = response.meta['book_title']
+        book_year = response.meta['book_year']
+        book_author = response.meta['book_author']
+        f_title = response.meta['f_title']
+        f_year = response.meta['f_year']
+
+        links = response.xpath('//a[@class="external text"]/@href').extract()
+        try:
+            imdb_url = list(filter(lambda x: re.search('www.imdb.com',x) != None,links))[-1]
+            
+            print('=='*50)
+            
+            yield Request(url=imdb_url, callback=self.parse_imdb_page,
+                            meta={'book_title': book_title,
+                                    'book_year': book_year,
+                                    'book_author': book_author,
+                                    'f_title': f_title,
+                                    'f_year': f_year})
+        except:
+            print('No imdb url')
+            print('X'*50)
+
+    def parse_imdb_page(self,response):
+        book_title = response.meta['book_title']
+        book_year = response.meta['book_year']
+        book_author = response.meta['book_author']
+        f_title = response.meta['f_title']
+        f_year = response.meta['f_year']
+
+        film_director = ", ".join(response.xpath('//div[@class="plot_summary "]/div[2]/a/text()').extract())
+        try:
+            film_avg_rating = float(response.xpath('//div[@class="ratingValue"]/strong/span/text()').extract_first())
+        except:
+            film_avg_rating = ''
+        try:
+            film_ratings = int(re.sub(',','',response.xpath('//span[@class="small"]/text()').extract_first()))
+        except:
+            film_ratings = ''
+        try:
+            film_meta_score = int(response.xpath('//div[@class="titleReviewBarItem"]/a/div/span/text()').extract_first())
+        except:
+            film_meta_score = ''
+        user_critic = response.xpath('//div[@class="titleReviewBarItem titleReviewbarItemBorder"]/div/span/a/text()').extract()
+        try:
+            film_user_reviews = int(re.sub('[\D]','',list(filter(lambda x: re.search('user',x) != None,user_critic))[0]))
+        except:
+            film_user_reviews = ''
+        try:
+            film_critic_reviews = int(re.sub('[\D]','',list(filter(lambda x: re.search('critic',x) != None,user_critic))[0]))
+        except:
+            film_critic_reviews = ''
+
+        for b_title,b_year in zip(book_title, book_year):
+            item = WikiBooksItem()
+            item['book_title'] = b_title
+            item['book_year'] = "-".join(b_year)
+            item['book_author'] = book_author
+            item['film_title'] = f_title
+            item['film_year'] =  "-".join(f_year)
+            item['film_director'] = film_director
+            item['film_avg_rating'] = film_avg_rating
+            item['film_ratings'] = film_ratings
+            item['film_meta_score'] = film_meta_score
+            item['film_user_reviews'] = film_user_reviews
+            item['film_critic_reviews'] = film_critic_reviews
+            yield item
